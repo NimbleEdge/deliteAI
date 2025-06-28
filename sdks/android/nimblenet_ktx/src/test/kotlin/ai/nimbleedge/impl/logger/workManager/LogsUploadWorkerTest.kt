db1aa58ffe22fc055dfc0f0ebbadcd7594e7cf1e
Naman Anand <naman.anand@nimbleedgehq.ai>
2025-06-28 10:48:08 +0530

    add headers Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>

--- sdks/android/nimblenet_ktx/src/test/kotlin/ai/nimbleedge/impl/logger/workManager/LogsUploadWorkerTest.kt
+++ sdks/android/nimblenet_ktx/src/test/kotlin/ai/nimbleedge/impl/logger/workManager/LogsUploadWorkerTest.kt
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2025 DeliteAI Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package ai.nimbleedge.impl.logger.workManager
 
 import ai.nimbleedge.datamodels.NimbleNetConfig
