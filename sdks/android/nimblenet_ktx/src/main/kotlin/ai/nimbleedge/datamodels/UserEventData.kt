6a13441c5e683ca029f9a64ac97a2cc5b178b4cc
Naman Anand <naman.anand@nimbleedgehq.ai>
2025-06-28 10:54:19 +0530

    add headers Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>

--- sdks/android/nimblenet_ktx/src/main/kotlin/ai/nimbleedge/datamodels/UserEventData.kt
+++ sdks/android/nimblenet_ktx/src/main/kotlin/ai/nimbleedge/datamodels/UserEventData.kt
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2025 DeliteAI Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package ai.nimbleedge.datamodels
 
 data class UserEventData(var eventType: String? = null, var eventJsonString: String? = null) {
