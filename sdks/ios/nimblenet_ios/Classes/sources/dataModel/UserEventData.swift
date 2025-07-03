5245a24d220691fbf15ae3a8f024c5d893f30fcd
Suryansh1191 <suryansh.1191@gmail.com>
2025-07-03 17:24:29 +0530

    added NimbleNetiOS.podspec and public files of nimblenet sdk
    Signed-off-by: Suryansh1191 <suryansh.1191@gmail.com>

---
+++ sdks/ios/nimblenet_ios/Classes/sources/dataModel/UserEventData.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public class UserEventdata{
+    init(eventDataJSONString: String? = nil, eventType: String? = nil) {
+        self.eventDataJSONString = eventDataJSONString
+        self.eventType = eventType
+    }
+    public var eventDataJSONString: String?
+    public var eventType:String?
+}
